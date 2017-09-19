9a6f06875b1b3dab77fe52c5c8a3cfcd6d5b844d
A.J. Murray <AJMJr910@gmail.com>
2017-09-19 16:17:53 -0400

    Spacing

--- Update-VMTools.ps1
+++ Update-VMTools.ps1
@@ -8,23 +8,19 @@ Connect-VIServer $vcenter
 $virtualmachines = Get-VM | Where-Object PowerState -eq "PoweredOn" | Select-Object -ExpandProperty Name
 
 #Get Only the Windows VMs
-ForEach ($vm in $virtualmachines)
-{
-#If the VM is a Windows VM...
-If (Get-VMGuest $vm | where-object OSFullName -like "Microsoft*")
-{
-#Update VMware Tools but don't reboot the machine
-Write-Host "Updating VMware tools on $vm"
-Update-Tools $vm -NoReboot
-}
-#If there is no OSFullName detected
-ElseIf(Get-VMGuest $vm | Where-Object OSFullName -eq $null)
-{
-Write-Host "VMware Tools doesn't appear to be installed on $vm"
-}
-#If it's not a Windows VM then automatic update is not supported
-else 
-{
-Write-Host "$vm is not a Windows VM and automatic Update is not supported"
-}
+ForEach ($vm in $virtualmachines){
+    #If the VM is a Windows VM...
+    If (Get-VMGuest $vm | where-object OSFullName -like "Microsoft*"){
+        #Update VMware Tools but don't reboot the machine
+        Write-Host "Updating VMware tools on $vm"
+        Update-Tools $vm -NoReboot
+    }
+    #If there is no OSFullName detected
+    ElseIf(Get-VMGuest $vm | Where-Object OSFullName -eq $null){
+    Write-Host "VMware Tools doesn't appear to be installed on $vm"
+    }
+    #If it's not a Windows VM then automatic update is not supported
+    else {
+    Write-Host "$vm is not a Windows VM and automatic Update is not supported"
+    }
 }
